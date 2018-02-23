d90c085ca0eb275dbccba2ee9342370f97ecf075
programer10 <programer10@ucione.local>
2018-02-23 21:05:04 +0100

    Create autoloader and function for XSS

---
+++ core/init.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
+
+session_start();
+
+spl_autoload_register(function ($class) {
+	require_once 'classes/' . $class  . '.php';
+});
+
+require_once 'functions/sanitize.php';
