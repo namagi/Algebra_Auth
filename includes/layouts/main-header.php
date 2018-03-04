a896a7390756a65fed83cdef2ac2fbb54c39d38b
namagi <br.mladen@gmail.com>
2018-03-04 11:00:57 +0100

    Add local BS css and js for ide completions

--- includes/layouts/main-header.php
+++ includes/layouts/main-header.php
@@ -8,7 +8,7 @@
     <title><?php echo $title ?></title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css">
+    <!--<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">-->
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
